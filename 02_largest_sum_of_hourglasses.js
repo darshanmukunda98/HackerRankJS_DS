64036ada49d1c72941ce218e7d6f4fbc827c6a43
darshanmukunda98 <22051109+darshanmukunda98@users.noreply.github.com>
2022-08-25 23:30:51 +0530

    Add files via upload

---
+++ 02_largest_sum_of_hourglasses.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'hourglassSum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+function hourglassSum(arr) {
+    // Write your code here
+    
+    let sums = [];
+    for(let i=0;i<=3;i++)
+    for(let j=0;j<=3;j++)
+    {
+        let sum = 0;
+        sum+=arr[i][j];
+        sum+=arr[i][j+1];
+        sum+=arr[i][j+2];
+        sum+=arr[i+1][j+1];
+        sum+=arr[i+2][j];
+        sum+=arr[i+2][j+1];
+        sum+=arr[i+2][j+2];
+        
+        sums.push(sum);
+    }
+    return (sums.length > 0) ? Math.max(...sums) : 0;
+    
+    
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    let arr = Array(6);
+
+    for (let i = 0; i < 6; i++) {
+        arr[i] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+
+    const result = hourglassSum(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
+
